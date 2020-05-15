8e82b0ca6e783ea7e437f2b14adac99fcd3da243
Henrique Ritter <henrique-ritter@hotmail.com>
2020-05-15 20:18:18 -0300

    appointments route created

--- module_02/primeiro_projeto_nodejs/src/routes/index.ts
+++ module_02/primeiro_projeto_nodejs/src/routes/index.ts
@@ -1,16 +1,10 @@
 import { Router } from 'express';
+import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
 
-routes.post('/users', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = {
-    name,
-    email,
-  };
-
-  return response.json(user);
-});
+routes.use('/appointments', appointmentsRouter);
+// Toda rota que iniciar com /appointment vai repassar o que vem após appointments
+// para dentro do appointmentsRouter(as rotas serao tratadas la no outro arquivo)
 
 export default routes;
