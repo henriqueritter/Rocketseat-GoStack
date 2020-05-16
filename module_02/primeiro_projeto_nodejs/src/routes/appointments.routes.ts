611b014962bde3911c85f857e6869336135cb5d7
Henrique Ritter <henrique-ritter@hotmail.com>
2020-05-16 17:34:51 -0300

    appointments model added

--- module_02/primeiro_projeto_nodejs/src/routes/appointments.routes.ts
+++ module_02/primeiro_projeto_nodejs/src/routes/appointments.routes.ts
@@ -1,18 +1,32 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+const appointments: Appointment[] = [];
+
+appointmentsRouter.get('/', (request, response) => {
+  return response.json(appointments);
+});
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
-  const appointment = {
-    id: uuid(), // generate uuid for provider
-    provider,
-    date,
-  };
+  const parsedDate = startOfHour(parseISO(date));
+
+  // Verifica se ha aglum agendamento na data especifica
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ msg: 'This appointment is already booked' });
+  }
+
+  const appointment = new Appointment(provider, parsedDate);
 
   appointments.push(appointment);
 
