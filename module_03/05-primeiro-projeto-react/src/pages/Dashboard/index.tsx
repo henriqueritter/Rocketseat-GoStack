08e3d43c743c4b0b36507f51cd2387f9a6f3e811
Henrique Ritter <henrique-ritter@hotmail.com>
2020-06-22 21:02:02 -0300

    styled-components

--- module_03/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ module_03/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Title } from './styles';
 
 //Usamos a funcao assim pois conseguimos passar a tipagem da funcao,
 //outra forma seria function Dashboard(){}
 //React.FC = React.FunctionComponent
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return <Title>Explore Repositorios no Github</Title>;
 };
 
 export default Dashboard;
