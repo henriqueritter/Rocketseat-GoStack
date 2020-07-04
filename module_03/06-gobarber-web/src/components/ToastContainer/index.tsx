5e8ae4ce1b4c43d34900ca1681ddf6d1bb9e3571
Henrique Ritter <henrique-ritter@hotmail.com>
2020-07-03 22:29:31 -0300

    Added icons and functions to toasts n timer.

--- module_03/06-gobarber-web/src/components/ToastContainer/index.tsx
+++ module_03/06-gobarber-web/src/components/ToastContainer/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { Container, Toast } from './styles';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
+import { Container } from './styles';
+import Toast from './Toast';
 
 //Interface importada
 import { ToastMessage, useToast } from '../../hooks/toast';
@@ -10,26 +10,10 @@ interface ToastContainerProps {
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
-  const { removeToast } = useToast();
-
   return (
     <Container>
       {messages.map((message) => (
-        <Toast
-          key={message.id}
-          type={message.type}
-          hasDescription={!!message.description} //Transforma o message description em boolean
-        >
-          <FiAlertCircle size={20} />
-
-          <div>
-            <strong>{message.title}</strong>
-            {message.description && <p>{message.description}</p>}
-          </div>
-          <button type="button" onClick={() => removeToast(message.id)}>
-            <FiXCircle size={18} />
-          </button>
-        </Toast>
+        <Toast key={message.id} message={message}></Toast>
       ))}
     </Container>
   );
