55e16d5e40de53acfb095043a3c5d66c2089a3dd
Henrique Ritter <henrique-ritter@hotmail.com>
2020-07-06 20:21:45 -0300

    Add logoff button on dashboard

--- module_03/06-gobarber-web/src/pages/Dashboard/index.tsx
+++ module_03/06-gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import { Container, Content } from './styles';
+
+import Button from '../../components/Button';
+import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  const history = useHistory();
+  const { signOut } = useAuth();
+
+  const handleLogoff = useCallback(() => {
+    signOut();
+    history.push('/');
+  }, [signOut, history]);
+
+  return (
+    <Container>
+      <Content>
+        <h1>DashBoard</h1>
+        <Button type="button" onClick={() => handleLogoff()}>
+          Sair
+        </Button>
+      </Content>
+    </Container>
+  );
 };
 
 export default Dashboard;
