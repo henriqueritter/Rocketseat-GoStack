bc117cfb6b4a38eeb2c2ac28e9ebdb4a36ea9970
Henrique Ritter <henrique-ritter@hotmail.com>
2020-08-29 11:10:48 -0300

    Dashboard init

--- module_03/07-appgobarber/src/pages/Dashboard/index.tsx
+++ module_03/07-appgobarber/src/pages/Dashboard/index.tsx
@@ -1,14 +1,56 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { View, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
 
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+  ProvidersList,
+} from './styles';
+
+interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const [providers, setProviders] = useState<Provider[]>([]);
+
+  const { signOut, user } = useAuth();
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    api.get('providers').then(response => {
+      setProviders(response.data);
+    });
+  }, []);
+
+  const navigateToProfile = useCallback(() => {
+    // navigation.navigate('Profile');
+    signOut();
+  }, [signOut]);
 
   return (
-    <View>
-      <Button title="Sair" onPress={signOut} />
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo,
+          {'\n'}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar source={{ uri: user.avatar_url }} />
+        </ProfileButton>
+      </Header>
+
+      <ProvidersList />
+    </Container>
   );
 };
 
--- module_03/07-appgobarber/src/routes/app.routes.tsx
+++ module_03/07-appgobarber/src/routes/app.routes.tsx
@@ -2,17 +2,27 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Dashboard from '../pages/Dashboard';
+import CreateAppointment from '../pages/CreateAppointment';
+import AppointmentCreated from '../pages/AppointmentCreated';
+
+import Profile from '../pages/Profile';
 
 const App = createStackNavigator();
 
 const AppRoutes: React.FC = () => (
   <App.Navigator
     screenOptions={{
-      // headerShown: false, // Parametro para remover o header das activity
+      headerShown: false, // Parametro para remover o header das activity
       cardStyle: { backgroundColor: '#312e38' },
     }}
   >
+    {/* Fluxo de criacao de appointments */}
     <App.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen name="CreateAppointment" component={CreateAppointment} />
+    <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+
+    {/* Fluxo de profile */}
+    <App.Screen name="Profile" component={Profile} />
   </App.Navigator>
 );
 
